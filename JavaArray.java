ece6857fd184c38942c120be9cbf76c6d410e855
Bulbul Ahammed <135580438+bulbulapex@users.noreply.github.com>
2024-10-22 20:29:21 +0600

    Array declare and print

--- JavaArray.java
+++ JavaArray.java
@@ -1,5 +1,10 @@
 public class JavaArray{
 	public static void main(String[] args) {
+		//Write a Java array that has three of your friend’s name
+		String[] frndsName = {"Rahim", "Karim", "Jamil"};
 		System.out.println("Hello World!");
+		System.out.println("My first friend name is: " + frndsName[0]);
+		System.out.println("My second friend name is: " + frndsName[1]);
+		System.out.println("My third friend name is: " + frndsName[2]);
 	}
 }
