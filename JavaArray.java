6e51df56a4a33aa3b67e1148b8c03f441a030bf6
Bulbul Ahammed <135580438+bulbulapex@users.noreply.github.com>
2024-10-22 20:37:24 +0600

    loop created for print first 10 nutural number

--- JavaArray.java
+++ JavaArray.java
@@ -6,5 +6,10 @@ public class JavaArray{
 		System.out.println("My first friend name is: " + frndsName[0]);
 		System.out.println("My second friend name is: " + frndsName[1]);
 		System.out.println("My third friend name is: " + frndsName[2]);
+
+		//2Write a Java loop that prints first 10 natural numbers.
+		for(int i = 1; i <= 10; i++){
+			System.out.println(i);
+		}
 	}
 }
